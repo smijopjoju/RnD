d5dc1c7b95895c41132abafc8c5d2ebedc71990a
smijopjoju <sj.2.3.4u@gmail.com>
2019-10-13 06:04:08 +0200

    find number of ways the chocklate can be shared.
    input array of integers s, the value indicate the each squre in a chocklate bar with an integers specified on it
    birth day of a person(d), and birth month (m)
    
    we need to find the max possible number ways for arranging the array
    
    condiftion
    find contigues value from the array whoes length is equal to the month (m)
    and some of the selected numbers should be equal to birth day (d)
    
    loop through the array,
    create an sub loop to loop through the array till the specified size
    calculate the sum of the sub loop elements and
    increment the counter value if the current loop satisfy the condition

---
+++ src/main/java/my/rnd/practice/hackerrank/ShareChocolateOnBirthday.java
@@ -0,0 +1,41 @@
+package my.rnd.practice.hackerrank;
+import java.lang.*;
+import java.util.Arrays;
+import java.util.List;
+
+public class ShareChocolateOnBirthday {
+    public static void main(String ...args) {
+            System.out.println(birthday(Arrays.asList(2, 2, 1, 3, 2), 4, 2));
+    }
+
+    static int birthday(List<Integer> s, int d, int m) {
+        int totalWaysToShareChocolate = 0;
+        int chocolatesSize = s.size();
+        int innerLoopSize = 0;
+        int sum = 0;
+        for (int outerLoopIndex = 0; outerLoopIndex < chocolatesSize; ++outerLoopIndex) {
+            innerLoopSize = outerLoopIndex + m;
+            if (innerLoopSize <= chocolatesSize) {
+                for (int innerLoopIndex = outerLoopIndex; innerLoopIndex < innerLoopSize; ++innerLoopIndex) {
+                    sum += s.get(innerLoopIndex);
+                }
+
+                if (sum == d) {
+                    ++totalWaysToShareChocolate;
+                }
+            } else {
+                if (m == 1 && s.size() == 1) {
+                    if (s.get(0) == d) {
+                        ++totalWaysToShareChocolate;
+                    }
+                }
+            }
+
+            sum = 0;
+        }
+
+        return totalWaysToShareChocolate;
+    }
+
+
+}
